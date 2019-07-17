d93c612baf91c8ca2293843a394745d895fc8c20
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2019-07-17 23:55:33 +0200

    Create wordpress-topics-op.meta.js

---
+++ src/wordpress-topics-op.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         WordPress.org Topics OP
+// @namespace    https://www.clorith.net
+// @version      0.1
+// @description  Mark topic OP on their replies for easy recognition.
+// @author       Clorith
+// @match        https://wordpress.org/support/topic/*
+// @match        https://*.wordpress.org/support/topic/*
+// @updateURL    https://github.com/wporg-support/topic-op/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// @downloadURL  https://github.com/wporg-support/topic-op/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// @grant        none
+// ==/UserScript==
